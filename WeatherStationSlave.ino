588d4d09e618a1d5670e973956b8ade41082a989
Paul <paul.beard@databaseconnect.co.uk>
2017-04-02 15:28:21 +0100

    Add files via upload

--- WeatherStationSlave.ino
+++ WeatherStationSlave.ino
@@ -16,26 +16,26 @@
 #define SERIAL_OUT 0
 
 #define WIND_GUST_PERIOD 6000UL
+#define COMPASS_DIRECTIONS 16
 
-long windDirTot = 0;
-long windDirCount = 0;
+//long windDirTot = 0;
+//long windDirCount = 0;
+long windDirTot[COMPASS_DIRECTIONS];
 unsigned int maxWindGustCount = 0;
 unsigned int windSpeedCount = 0;
 unsigned int rainCount = 0;
 
-long windDirPrev = 0;
+//long windDirPrev = 0;
 unsigned int windGustCount = 0;
 byte windSpeedEdgeCount = 0;
 int prevWindSpeedSensor = LOW;
 int prevRainSensor = LOW;
 
 unsigned long lastWindGustTime;
-unsigned long lastWindDirTime;
 unsigned long lastReportTime;
-bool resetReqd = false;
 
-const int reading[] = {147, 172, 208, 281, 368, 437, 531, 624, 710, 780, 817, 870, 909, 938, 970, 1023};
-const int compass[] = {112,  67,  90, 157, 135, 202, 180,  22,  45, 247, 225, 337,   0, 292, 315,  270};
+const int reading[COMPASS_DIRECTIONS] = {147, 172, 208, 281, 368, 437, 531, 624, 710, 780, 817, 870, 909, 938, 970, 1023};
+const int compass[COMPASS_DIRECTIONS] = {112,  67,  90, 157, 135, 202, 180,  22,  45, 247, 225, 337,   0, 292, 315,  270};
 
 void setup() {
 
@@ -72,11 +72,24 @@ void loop() {
       // Has line been high & stable ?
       if (millis() > endTime) {
 
+        // Find most common wind direction over the reporting period
+        long maxWindDirTot = 0;
+        byte maxWindDirIndex = 0;
+        for (byte i = 0; i < COMPASS_DIRECTIONS; i++) {
+          if (windDirTot[i] > maxWindDirTot) {
+            maxWindDirTot = windDirTot[i];
+            maxWindDirIndex = i;
+          }
+          //Reset for next reporting period
+          windDirTot[i] = 0;
+        }
+        int windDir = compass[maxWindDirIndex];
+
         // Calculate average wind direction over the reporting period
-        int windDir;
-        if (windDirCount > 0) windDir = windDirTot / windDirCount; else windDir = 0;
-        while (windDir >= 360) windDir -= 360;
-        while (windDir < 0) windDir += 360;
+        //int windDir;
+        //if (windDirCount > 0) windDir = windDirTot / windDirCount; else windDir = 0;
+        //while (windDir >= 360) windDir -= 360;
+        //while (windDir < 0) windDir += 360;
   
         // Send all sensor data to master
         pinMode(SERIAL_OUT, OUTPUT);
@@ -94,8 +107,8 @@ void loop() {
         pinMode(SERIAL_OUT, INPUT); // INPUT_PULLUP not needed because Wemos D3 has built-in pullup
   
         // Zero counters for next reporting period
-        windDirTot = 0;
-        windDirCount = 0;
+        //windDirTot = 0;
+        //windDirCount = 0;
         windSpeedCount = 0;
         maxWindGustCount = 0;
         rainCount = 0;
@@ -106,40 +119,17 @@ void loop() {
   // Check Wind speed sensor
   pinMode(WIND_SPEED_SENSOR, INPUT_PULLUP);
   int currWindSpeedSensor = digitalRead(WIND_SPEED_SENSOR);
+  pinMode(WIND_SPEED_SENSOR, INPUT);
   if (currWindSpeedSensor != prevWindSpeedSensor) {
     windSpeedEdgeCount++;
-    if (windSpeedEdgeCount > 3) {
+    if (windSpeedEdgeCount > 1) {
       windSpeedEdgeCount = 0;
       windSpeedCount++;
     }
     windGustCount++;
     prevWindSpeedSensor = currWindSpeedSensor;
-  }
-  pinMode(WIND_SPEED_SENSOR, INPUT);
 
-  // Check rain sensor
-  pinMode(RAIN_SENSOR, INPUT_PULLUP);
-  int currRainSensor = digitalRead(RAIN_SENSOR);
-  if (currRainSensor != prevRainSensor) {
-    if (prevRainSensor == HIGH) rainCount++;
-    prevRainSensor = currRainSensor;
-  }
-  pinMode(RAIN_SENSOR, INPUT);
-
-  // Time to check for wind gust?
-  unsigned long windGustPeriod = timeNow - lastWindGustTime;
-  if (windGustPeriod >= WIND_GUST_PERIOD) {
-    lastWindGustTime += WIND_GUST_PERIOD;
-    // New highest wind gust?
-    if (windGustCount > maxWindGustCount) maxWindGustCount = windGustCount;
-    // Zero gust count for next period
-    windGustCount = 0;
-  }
-
-  // Time to check wind direction sensor?
-  if (timeNow - lastWindDirTime >= 200) {
-    lastWindDirTime += 200;
-
+    // Now check the wind direction sensor
     // Enable sensor only to take reading. Reduces current
     bitClear(PRR, PRADC); // power-on ADC
     bitSet(ADCSRA, ADEN); // Enable ADC
@@ -153,16 +143,36 @@ void loop() {
     // Can be one of 16 values, but there could be some
     // analog noise, so the reading[] array contains values
     // mid-way between the expected readings.
-    int i;
-    for (i = 0; i < 16 && readingNow >= reading[i]; i++);
-    int windDirNow = compass[i];
+    byte i;
+    for (i = 0; i < COMPASS_DIRECTIONS && readingNow >= reading[i]; i++);
+    windDirTot[i]++;
+    //int windDirNow = compass[i];
     // Check if sensor has swept through 0 degrees, moving in either direction
-    if (windDirNow - windDirPrev > 180) windDirNow -= 360;
-    if (windDirPrev - windDirNow > 180) windDirNow += 360;
+    //if (windDirNow - windDirPrev > 180) windDirNow -= 360;
+    //if (windDirPrev - windDirNow > 180) windDirNow += 360;
     // Update total and count of data points for calculating average
-    windDirTot += windDirNow;
-    windDirCount++;
-    windDirPrev = windDirNow;
+    //windDirTot += windDirNow;
+    //windDirCount++;
+    //windDirPrev = windDirNow;
+  }
+
+  // Check rain sensor
+  pinMode(RAIN_SENSOR, INPUT_PULLUP);
+  int currRainSensor = digitalRead(RAIN_SENSOR);
+  pinMode(RAIN_SENSOR, INPUT);
+  if (currRainSensor != prevRainSensor) {
+    if (prevRainSensor == HIGH) rainCount++;
+    prevRainSensor = currRainSensor;
+  }
+
+  // Time to check for wind gust?
+  unsigned long windGustPeriod = timeNow - lastWindGustTime;
+  if (windGustPeriod >= WIND_GUST_PERIOD) {
+    lastWindGustTime += WIND_GUST_PERIOD;
+    // New highest wind gust?
+    if (windGustCount > maxWindGustCount) maxWindGustCount = windGustCount;
+    // Zero gust count for next period
+    windGustCount = 0;
   }
 
   // Go to sleep until next 1ms timer interrupt
